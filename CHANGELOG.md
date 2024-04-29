44281f1da52678bb1e445a5aea26a93d7497540c
MariooC14 <v.caval1@universityofgalway.ie>
2024-04-29 21:40:31 +0100

    Added Prettier and reformatted files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,9 @@
-Project change log
-==================
+# Project change log
 
 ### 0.0.2 2024-04-29
-* Set up firebase, Shadcn UI and other dependencies
+
+- Set up firebase, Shadcn UI and other dependencies
 
 ### 0.0.1 2024-04-27
-* Initial setup of the project
+
+- Initial setup of the project
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './index.tsx'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Index from "./index.tsx";
+import "./main.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./main.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Index />,
-  },
+	{
+		path: "/",
+		element: <Index />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
+);
