c133cc9e520bb3abd90f0690b05f4eb1a0547ea5
David Wilson <szabol.david04@gmail.com>
2024-04-30 19:58:33 +0200

    stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Index from "./index.tsx";
-import "./main.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Index from './index.tsx'
+import LoginForm from './login.tsx'
+import './main.css'
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import "./main.css";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Index />,
-	},
+    {
+        path: "/",
+        element: <Index/>,
+    },
+    {
+        path: "/login",
+        element: <LoginForm/>,
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+)
+
