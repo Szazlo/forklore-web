5b56c04d1522c6dbf2377fcd4252b9c010b59eed
David Wilson <szabol.david04@gmail.com>
2024-05-01 17:44:52 +0200

    login functionality

--- src/login.tsx
+++ src/login.tsx
@@ -2,25 +2,29 @@ import "./main.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
+import { GoogleAuthProvider } from "firebase/auth";
 
 library.add(fab)
 
 function LoginForm() {
+    const googleProvider = new GoogleAuthProvider();
+    googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
     return (
-        <div className="lg:flex h-screen bg-primary">
-            <div className="w-full bg-white flex items-start justify-start p-4 lg:hidden">
+        <div className="flex flex-col lg:flex-row h-screen bg-primary">
+            <div className="bg-white items-center p-4 lg:hidden">
                 <img src='/public/logo.png' alt="Forklore logo" className="h-10 mx-auto"/>
             </div>
-            <div className="hidden lg:block lg:w-1/2 bg-secondary flex items-start justify-start p-4">
+            <div className="hidden lg:block lg:w-1/2 bg-secondary items-start justify-start p-4">
                 <img src='/public/logo.png' alt="Forklore logo" className="hidden lg:block h-10"/>
             </div>
-            <div className="w-full lg:w-1/2 bg-white flex items-center justify-center">
+            <div className="w-full lg:w-1/2 bg-white items-center">
                 <div className="flex items-center justify-center h-screen">
-                    <form className="flex flex-col w-4/5 max-w-lg mx-auto items-center">
+                    <form className="flex flex-col w-4/5 max-w-lg mx-auto w-3/5 items-center">
                         <h2 className="mb-4 text-2xl text-primary">Login</h2>
-                        <input className="mb-4 w-full p-2 border border-primary rounded-full" type="text"
+                        <input className="mb-4 w-2/3 p-2 border border-primary rounded-full" type="text"
                                placeholder="Username" required/>
-                        <input className="mb-4 w-full p-2 border border-primary rounded-full" type="password"
+                        <input className="mb-4 w-2/3 p-2 border border-primary rounded-full" type="password"
                                placeholder="Password" required/>
                         <button className="w-1/2 p-2 bg-primary text-white rounded-full hover:bg-accent"
                                 type="submit">Submit
@@ -33,19 +37,19 @@ function LoginForm() {
                         </div>
                         <p className="mt-4 text-primary">Login with:</p>
                         <div className="flex w-full justify-center mt-4">
-                            <a href="/auth/facebook" className="mx-2 login-icon">
+                            <a href="#" className="mx-2 login-icon">
                                 <FontAwesomeIcon icon={['fab', 'facebook']} size="2x"/>
                             </a>
-                            <a href="/auth/google" className="mx-2 login-icon">
+                            <a href="#" className="mx-2 login-icon">
                                 <FontAwesomeIcon icon={['fab', 'google']} size="2x"/>
                             </a>
-                            <a href="/auth/github" className="mx-2 login-icon">
+                            <a href="#" className="mx-2 login-icon">
                                 <FontAwesomeIcon icon={['fab', 'microsoft']} size="2x"/>
                             </a>
-                            <a href="/auth/twitter" className="mx-2 login-icon">
+                            <a href="#" className="mx-2 login-icon">
                                 <FontAwesomeIcon icon={['fab', 'twitter']} size="2x"/>
                             </a>
-                            <a href="/auth/apple" className="mx-2 login-icon">
+                            <a href="#" className="mx-2 login-icon">
                                 <FontAwesomeIcon icon={['fab', 'apple']} size="2x"/>
                             </a>
                         </div>
