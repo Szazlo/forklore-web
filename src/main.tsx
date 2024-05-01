beaf0b19cfd05d91a82e050b446174c8f38ff542
David Wilson <szabol.david04@gmail.com>
2024-05-01 19:32:16 +0200

    [Important] Implemented Key main navbar feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Index from "./index.tsx";
 import "./main.css";
+import "@fontsource/inter";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./main.css";
