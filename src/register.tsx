9f634dfb0ceeb612eabf1c124c97912afc18a0f1
laplace <lauri.j.kiukkonen@gmail.com>
2024-05-04 20:15:37 +0300

    sign up brnach created

--- src/register.tsx
+++ src/register.tsx
@@ -3,6 +3,12 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 // import { selectUser } from "./store";
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import {FormEvent, useState} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "@/store/auth/authSlice.ts";
+import {useNavigate} from "react-router-dom";
 
 library.add(fab)
 
@@ -20,21 +26,28 @@ function SignUpForm() {
                     <form className="flex flex-col w-3/5 max-w-lg mx-auto items-center">
                         <h2 className="mb-4 text-2xl text-primary">Sign up</h2>
                         <div className="flex flex-row items-stretch justify-center w-full">
-                            <input className="mb-4 mr-1 px-4 py-2 w-full p-2 border border-primary rounded-full" type="text"
-                                   placeholder="First Name" required/>
-                            <input className="mb-4 ml-1 px-4 py-2 w-full p-2 border border-primary rounded-full" type="text"
-                                   placeholder="Last Name" required/>
+                            {/* First name field*/}
+                            <input name="firstName" className="mb-4 mr-1 px-4 py-2 w-full p-2 border border-primary rounded-full"
+                                   type="text"
+                                   placeholder="First Name" />
+                            {/* Last Name Field*/}
+                            <input name="lastName" className="mb-4 ml-1 px-4 py-2 w-full p-2 border border-primary rounded-full"
+                                   type="text"
+                                   placeholder="Last Name" />
                         </div>
                         <input className="mb-4 w-full px-4 py-2 border border-primary rounded-full" type="text"
                                placeholder="Username" required/>
-                        <input className="mb-4 w-full px-4 py-2 border border-primary rounded-full" type="password"
+                        <input name={"email"} className="mb-4 w-full px-4 py-2 border border-primary rounded-full" type="text"
+                               placeholder="Email" required/>
+                        <input name={"password"} className="mb-4 w-full px-4 py-2 border border-primary rounded-full" type="password"
                                placeholder="Password" required/>
                         <button className="w-1/2 p-2 bg-primary text-white rounded-full hover:bg-accent"
                                 type="submit">Sign up
                         </button>
                     </form>
-                    <a className="mt-4 text-primary hover:text-accent hover:underline" href="/login">Already have an account? Sign in</a>
-                        <div className="flex justify-center items-center w-full mt-4">
+                    <a className="mt-4 text-primary hover:text-accent hover:underline" href="/login">Already have an
+                        account? Sign in</a>
+                    <div className="flex justify-center items-center w-full mt-4">
                             <hr className="w-1/4 border-t border-secondary"/>
                             <p className="mx-4 text-secondary">OR</p>
                             <hr className="w-1/4 border-t border-secondary"/>
