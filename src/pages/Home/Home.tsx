f28f135e43a064c2404de2b4869693ca1644b445
Mario C <cavalmario2003@gmail.com>
2024-06-25 20:22:44 +0100

    Removed Blob that causes document size errors

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -63,12 +63,12 @@ function Home() {
 	const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 	const isTablet = useMediaQuery(theme.breakpoints.down("md"));
 
-	const recipeCards = recipes.map((recipeData: RecipeCardData & any) => <RecipeCard key={recipeData.id} {...recipeData} />);
+	const recipeCards = recipes.map((recipeData: RecipeCardData) => <RecipeCard key={recipeData.id} {...recipeData} />);
 
 	return (
 		<>
 			<Container maxWidth="lg">
-				<Blob /> {/* That green yolk on the top right of the page*/}
+				{/* <Blob /> That green yolk on the top right of the page */}
 				<Container sx={{ textAlign: isTablet ? "center": "left", my: 10 }}>
 					<Grid container>
 						<Grid item xs={12} md={6}>
