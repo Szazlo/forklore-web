418eb06c9ef9870dbd276217d7097468b7cfb468
David Wilson <szabol.david04@gmail.com>
2024-07-08 19:46:17 +0200

    Init

---
+++ src/pages/Feed/Feed.tsx
@@ -0,0 +1,14 @@
+import "@/main.css";
+import {Container} from "@mui/material";
+
+function Recipe() {
+    return (
+        <>
+            <Container maxWidth={"lg"}>
+                text
+            </Container>
+        </>
+    );
+}
+
+export default Recipe;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,6 +4,7 @@ import PageNotFound from "@/pages/404";
 import SignUpForm from "@/pages/register";
 import Recipe from "@/pages/Recipe";
 import Root from "@/components/Root.tsx";
+import Feed from "@/pages/Feed";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -27,6 +28,10 @@ export const router = createBrowserRouter([
 			{
 				path: "/signup",
 				element: <SignUpForm />,
+			},
+			{
+				path: "/feed",
+				element: <Feed />,
 			},
 			{
 				path: "*",
