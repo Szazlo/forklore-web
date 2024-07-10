863aa6ddf13187d8712c78eba7a12001b1c2ae03
laplace <lauri.j.kiukkonen@gmail.com>
2024-07-10 20:34:35 +0300

    added snackbar context provider and hook

---
+++ src/context/SnackbarProvider/SnackbarProvider.tsx
@@ -0,0 +1,44 @@
+import { Snackbar, AlertColor, Alert} from "@mui/material";
+import {createContext, ReactNode, useState} from "react";
+
+interface snackContextValues {
+    addSnack: (message: string, severity?: AlertColor) => void;
+}
+
+interface Snack {
+    message: string;
+    severity?: AlertColor
+}
+
+export const SnackbarContext = createContext({} as snackContextValues);
+
+export function SnackbarProvider({ children }: { children: ReactNode }) {
+    const [open, setOpen] = useState(false)
+    const [snack, setSnack] = useState<Snack>({
+        message: '',
+        severity: 'success'
+    })
+
+    const handleClose = (_event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') return
+        setOpen(false);
+    }
+
+    const addSnack = (message: string, severity?: AlertColor) => {
+        setSnack({ message, severity: severity || 'success' });
+        setOpen(true);
+    };
+
+    const value = { addSnack };
+
+    return (
+        <SnackbarContext.Provider value={value}>
+            <Snackbar autoHideDuration={4000} open={open} onClose={handleClose}>
+                <Alert severity={snack.severity} onClose={handleClose}>
+                    {snack.message}
+                </Alert>
+            </Snackbar>
+            {children}
+        </SnackbarContext.Provider>
+    )
+}
