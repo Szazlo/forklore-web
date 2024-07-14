a22bbdb7cd32b9ba5ae452ce85a4d14550cf5a5b
David Wilson <szabol.david04@gmail.com>
2024-07-14 17:33:41 +0200

    recipe cards rework

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -1,54 +1,85 @@
 import { RecipeCardData } from "@/types/recipe";
-import { Avatar, Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, IconButton, Rating, Typography, useTheme } from "@mui/material";
-import { useMediaQuery } from "@mui/material";
+import {
+    Box,
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Chip,
+    Grid,
+    IconButton,
+    Typography
+} from "@mui/material";
 import { MouseEvent, useState } from "react";
-import WhatshotIcon from '@mui/icons-material/Whatshot';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { Star as StarIcon } from "lucide-react";
 
 export default function RecipeCard(props: RecipeCardData & any) {
-  const [bookmarked, setBookmarked] = useState(false);
-  const theme = useTheme();
-  const isTablet = useMediaQuery(theme.breakpoints.down("md"));
+    const [bookmarked, setBookmarked] = useState(false);
 
-	function handleBookmarkToggle(event: MouseEvent) {
-		event.stopPropagation();
-		event.preventDefault();
-		setBookmarked(prev =>  !prev);
-		// Save or unsave here to database
-	}
+    function handleBookmarkToggle(event: MouseEvent) {
+        event.stopPropagation();
+        event.preventDefault();
+        setBookmarked(prev => !prev);
+    }
 
-  return (
-		<Grid item xs={6} sm={4} >
-      <Card sx={{ borderRadius: 3, position: "relative" }}>
-        <CardActionArea>
-          <CardMedia sx={{ borderRadius: 3, objectFit: "cover" }} component="img" image={props?.image} alt={props.title} />
-          <CardContent sx={{ p: isTablet ? 1 : 2 }}>
-          <Rating size="small" readOnly defaultValue={2.5} precision={0.5} />
-            <Typography minHeight={50}>{props.title}</Typography>
+    return (
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Card sx={{ borderRadius: 3, position: "relative" }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        image={props?.image}
+                        alt={props.title}
+                        sx={{
+                            borderRadius: 3,
+                            objectFit: "cover",
+                            borderBottomRightRadius: 0,
+                            borderBottomLeftRadius: 0,
+                            height: { xs: 150, sm: 200, md: 250 }
+                        }}
+                    />
+                    <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'start', mb: 2 }}>
+                        <Box sx={{ flex: 2 }}>
+                            <Typography variant="body1" color="text.primary" fontWeight={"bold"}>
+                                {props.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Description
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {props.publisher.firstName} {props.publisher.lastName}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ border: 1, borderColor: 'primary.main', borderRadius: 1, p: 1, marginLeft: 2}}>
+                            <Typography textAlign="center" variant="body2" color="primary">
+                                {props.cookingTime}
+                            </Typography>
+                            <Typography textAlign="center" variant="body2" color="primary">
+                                min
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2 }}>
+                        <Box>
+                            <Chip variant="outlined" size="small" label="Indian" sx={{ mr: 1 }} />
+                            <Chip variant="outlined" size="small" label="African" sx={{ mr: 1 }} />
+                            <Chip variant="outlined" size="small" label="Fusion" />
+                        </Box>
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                            <StarIcon size={16} color="gold" />
+                            <Typography variant="body2" sx={{ ml: 0.5 }}>{props.averageRating}</Typography>
+                        </Box>
+                    </Box>
+                </CardActionArea>
 
-            {/* Publisher and Calories */}
-            {!isTablet &&
-              <div className="flex justify-between items-center">
-								<div className="flex items-center gap-1">
-									<Avatar sx={{ width: 32, height: 32, bgcolor: "primary.main" }}>D</Avatar>
-									<Typography variant="body2" color="text.secondary">{props.publisher.firstName?.at(0) + ". " + props.publisher.lastName}</Typography>
-								</div>
-
-								<Button disableRipple size="small" variant="outlined" color="warning" startIcon={<WhatshotIcon />}>251 cals</Button>
-
-              </div>
-            }
-          </CardContent>
-        </CardActionArea>
-
-        {/* Bookmark Button */}
-        <Box position="absolute" top={4} right={4} bgcolor="white" borderRadius={3}>
-          <IconButton onClick={handleBookmarkToggle} color="primary" sx={{ p: 0.8 }}>
-            {bookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
-          </IconButton>
-        </Box>
-      </Card>
-    </Grid>
-  );
+                <Box position="absolute" top={4} right={4} bgcolor="white" borderRadius={3}>
+                    <IconButton onClick={handleBookmarkToggle} color="primary" sx={{ p: 0.8 }}>
+                        {bookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+                    </IconButton>
+                </Box>
+            </Card>
+        </Grid>
+    );
 }
