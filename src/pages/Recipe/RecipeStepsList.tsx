ce5f5ddf07caa41cb5b1ef8fe3bbac2e09de14f5
Mario Caval <cavalmario2003@gmail.com>
2024-08-09 17:00:35 +0100

    Changed the recipe steps so only clicking on the step number will cross the step off.

--- src/pages/Recipe/RecipeStepsList.tsx
+++ src/pages/Recipe/RecipeStepsList.tsx
@@ -2,7 +2,6 @@ import { Box, Stack, Typography } from "@mui/material";
 import { RecipeInstruction } from "@/types/recipe";
 import { useToggle } from "@/useToggle.ts";
 import DoneIcon from "@mui/icons-material/Done";
-import { MouseEvent, useState } from "react";
 
 export default function RecipeStepsList({ steps }: { steps: RecipeInstruction[] }) {
 
@@ -17,25 +16,11 @@ export default function RecipeStepsList({ steps }: { steps: RecipeInstruction[] 
 
 function RecipeStep({ step }: { step: RecipeInstruction }) {
 	const [ticked, toggleTicked] = useToggle(false);
-	const [wasSelecting, setWasSelecting] = useState(false);
-
-	const handleClick = (e: MouseEvent) => {
-		// If the user is selecting text, do not propagate to trigger the click event
-		console.log(getSelection()?.toString());
-		if (window.getSelection()?.toString()) {
-			e.preventDefault();
-			e.stopPropagation();
-			setWasSelecting(true);
-		} else if (wasSelecting) {
-			toggleTicked();
-			setWasSelecting(false);
-		}
-	};
 
 	return (
-		<div className="flex gap-1" onClick={handleClick}>
+		<div className="flex gap-1">
 			<div className="w-8">
-				<Box bgcolor="primary.main"
+				<Box bgcolor="primary.main" onClick={toggleTicked}
 						 className="w-6 h-6 flex align-middle justify-center rounded text-white cursor-pointer select-none">
 					{ticked ? <DoneIcon /> : step.stepNumber}
 				</Box>
