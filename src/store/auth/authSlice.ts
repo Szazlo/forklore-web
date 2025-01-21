a27ab73c28fcdc73af3d7d1ed2ec494ef594acbc
Mario Caval <cavalmario2003@gmail.com>
2025-01-21 22:56:39 +0000

    Added persistent auth with localstorage

--- src/store/auth/authSlice.ts
+++ src/store/auth/authSlice.ts
@@ -1,30 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '../store';
-import { ForkloreUser } from '@/types/User';
-import { Logger } from '@/lib/logger';
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import { ForkloreUser } from "@/types/User";
+import { Logger } from "@/lib/logger";
 
 export interface AuthStateSlice {
-    user: ForkloreUser | null;
-    token: string;
+	user: ForkloreUser | null;
 }
+
+const userFromStorage: ForkloreUser = JSON.parse(
+	localStorage.getItem("auth_user") as string,
+);
 
 const initialState: AuthStateSlice = {
-    user: null,
-    token: "",
-}
+	user: userFromStorage || null,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login: (state, action) => {
-            state.user = action.payload;
-            authLogger.log("Signed in as", state.user, );
-        },
-        logout: (state) => {
-            state.user = null;
-        },
-    },
+	name: "auth",
+	initialState,
+	reducers: {
+		login: (state, action) => {
+			state.user = action.payload;
+			localStorage.setItem("auth_user", JSON.stringify(state.user));
+			authLogger.log("Signed in as", state.user);
+		},
+		logout: (state) => {
+			state.user = null;
+			localStorage.removeItem("auth_user");
+		},
+	},
 });
 
 export const authLogger = new Logger("Auth");
