417e78f91b1ba08ce0910058b4f319dad455c31a
Mario C <cavalmario2003@gmail.com>
2025-02-23 20:55:50 +0000

    Updated react router to 7.2.0 and refactored app routing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import "./main.css";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./main.css";
 import { ColorModeProvider } from "@/context/ColorModeProvider";
 import { SnackbarProvider } from "@/context/SnackbarProvider";
-import { router } from "./router";
+import Root from "./components/Root";
+import Home from "./pages/Home";
+import Feed from "./pages/Feed";
+import PageNotFound from "./pages/404";
+import EmailVerified from "./pages/EmailVerified";
+import Recipe from "./pages/Recipe";
+import LoginForm from "./pages/login";
+import RecipeEditor from "./pages/RecipeEditor";
+import SignUpPage from "./pages/register";
 
 export default function App() {
 	return (
 		<ColorModeProvider>
 			<SnackbarProvider>
-				<RouterProvider router={router} />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Root />}>
+							<Route index element={<Home />} />
+							<Route path="/recipe" element={<Recipe />} />
+							<Route path="/recipecreator" element={<RecipeEditor />} />
+							<Route path="/login" element={<LoginForm />} />
+							<Route path="/signup" element={<SignUpPage />} />
+							<Route path="/feed" element={<Feed />} />
+							<Route path="/emailVerified" element={<EmailVerified />} />
+							<Route path="*" element={<PageNotFound />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
 			</SnackbarProvider>
 		</ColorModeProvider>
 	);
